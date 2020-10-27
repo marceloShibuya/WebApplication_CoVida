6c792b0cbd3899d669a926460a963789b115c78d
FelipeOliver00 <Felipecastro0208@hotmail.com>
2020-10-26 22:08:37 -0300

    Limitação de visualizações = 5

--- CoVida/src/main/java/br/com/fiap/CoVida/controller/ContatoController.java
+++ CoVida/src/main/java/br/com/fiap/CoVida/controller/ContatoController.java
@@ -6,12 +6,17 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,8 +31,9 @@ public class ContatoController {
 	private ContatoRepository repository;
 
 	@GetMapping()
-	public ModelAndView contatos() {
-		List<Contato> contatos = repository.findAll();
+	public ModelAndView contatos(@PageableDefault(page =0, size=5)Pageable pageable) {
+	
+		Page<Contato> contatos = repository.findAll(pageable);
 		ModelAndView modelAndView = new ModelAndView("contatos");
 		modelAndView.addObject("contatos", contatos);
 		return modelAndView;
@@ -66,7 +72,7 @@ public class ContatoController {
 	public String updateContato(@Valid Contato contato, BindingResult result, RedirectAttributes attributes){
 		if (result.hasErrors()) return "contato_edit";
 		repository.save(contato);
-		attributes.addFlashAttribute("message", "contato atualizado com sucesso");
+		attributes.addFlashAttribute("message", "contato editado com sucesso");
 		return "redirect:/contato";
 	}
 	
